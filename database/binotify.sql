6bafa6053995b991ad5b0966c0f823bc9ba43a2e
Farrel Ahmad <13520110@std.stei.itb.ac.id>
2022-10-22 05:50:09 +0700

    Add records example

--- database/binotify.sql
+++ database/binotify.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `album`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `album` (
-  `album_id` int NOT NULL,
+  `album_id` int NOT NULL AUTO_INCREMENT,
   `judul` char(64) NOT NULL,
   `penyanyi` char(128) NOT NULL,
   `total_duration` int NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE `album` (
   `tanggal_terbit` date NOT NULL,
   `genre` char(64) DEFAULT NULL,
   PRIMARY KEY (`album_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `album` (
 
 LOCK TABLES `album` WRITE;
 /*!40000 ALTER TABLE `album` DISABLE KEYS */;
+INSERT INTO `album` VALUES (1,'Heavy Rotation','JKT48',2518,'/heavy_rotation/album.jpg','2013-02-16','J-pop'),(2,'Kereta Kencan','HiVi!',2110,'/kereta_kencan/album.jpg','2017-02-23','Pop'),(3,'RIVER','JKT48',1052,'/river/album.jpg','2013-05-11','J-pop');
 /*!40000 ALTER TABLE `album` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,7 +52,7 @@ DROP TABLE IF EXISTS `song`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `song` (
-  `song_id` int NOT NULL,
+  `song_id` int NOT NULL AUTO_INCREMENT,
   `judul` char(64) NOT NULL,
   `penyanyi` char(128) DEFAULT NULL,
   `tanggal_terbit` date NOT NULL,
@@ -63,7 +64,7 @@ CREATE TABLE `song` (
   PRIMARY KEY (`song_id`),
   KEY `album_id` (`album_id`),
   CONSTRAINT `song_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +73,7 @@ CREATE TABLE `song` (
 
 LOCK TABLES `song` WRITE;
 /*!40000 ALTER TABLE `song` DISABLE KEYS */;
+INSERT INTO `song` VALUES (1,'Heavy Rotation','JKT48','2013-02-16','J-pop',283,'/heavy_rotation/heavy_rotation.mp4','/heavy_rotation/heavy_rotation.jpg',1),(2,'Baby! Baby! Baby!','JKT48','2013-02-16','J-pop',242,'/heavy_rotation/baby!_baby!_baby!.mp4','/heavy_rotation/baby!_baby!_baby!.jpg',1),(3,'Remaja','HiVi!','2017-02-23','Pop',218,'/kereta_kencan/remaja.mp4','/kereta_kencan/remaja.jpg',2),(4,'RIVER','JKT48','2013-05-11','J-pop',281,'/river/river.mp4','/river/river.jpg',3);
 /*!40000 ALTER TABLE `song` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,13 +85,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `user_id` int NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `email` char(255) NOT NULL,
   `password` char(255) NOT NULL,
   `username` char(255) NOT NULL,
   `isAdmin` tinyint(1) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,6 +100,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin@example.com','admin','admin',1),(2,'user@example.com','user','user',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -110,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-22  4:58:48
+-- Dump completed on 2022-10-22  5:49:16
