6b25cb9ac8ed371f896e19ab38855c8c543c64aa
Ignasius Ferry <13520126@std.stei.itb.ac.id>
2022-10-22 23:00:40 +0700

    Fix routing bug

--- App/core/Router.php
+++ App/core/Router.php
@@ -8,7 +8,7 @@ class Router {
         // Iterates through routes to find a handler for the corresponding route
         $path = "";
         if (isset($_SERVER["REQUEST_URI"])) {
-            $path = ltrim($_SERVER["REQUEST_URI"], '/');
+            $path = ltrim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), '/');
         }
 
         $method = $_SERVER["REQUEST_METHOD"];
